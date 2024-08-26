23c6f7ddf7df0950c03c87f04775a713481622c4
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-08-26 22:05:55 +0700

    home, display user group chat and button create new group

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Link, useOutletContext } from 'react-router-dom';
+import { Link, useNavigate, useOutletContext } from 'react-router-dom';
+import api from '../configs/api';
 
 function App() {
+  const navigate = useNavigate();
   const [user] = useOutletContext();
   const [users, setUsers] = useState([]);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     if (!user) {
@@ -14,10 +17,39 @@ function App() {
     fetch(`${process.env.REACT_APP_SERVER_URL}/user/${user.id}/others`)
       .then((res) => res.json())
       .then((json) => setUsers(json));
+
+    api.get(`/user/${user.id}`).then((res) => {
+      setUserData(res.data);
+      console.log(res.data);
+    });
   }, [user]);
+
+  const onClickCreateNewGroupChat = async () => {
+    api.post('/chat/group', { id: user.id }).then((res) => {
+      if (!res.data.success) {
+        console.error('fail to create group chat');
+        return;
+      }
+      navigate(`/chat/group/${res.data.data.id}`);
+    });
+  };
+
+  if (!user) return <p>Please login</p>;
+
   return (
     <>
-      {user ? <h1>Users</h1> : <p>Please login</p>}
+      <h1>Group chats</h1>
+      <button onClick={onClickCreateNewGroupChat}>Create new group chat</button>
+      {userData && userData.groupChats.length > 0 && (
+        <div>
+          {userData.groupChats.map((group) => (
+            <div key={group.id}>
+              <Link to={`/chat/group/${group.id}`}>{group.id}</Link>
+            </div>
+          ))}
+        </div>
+      )}
+      <h1>All Users</h1>
       {user &&
         users.length > 0 &&
         users.map((user) => (
